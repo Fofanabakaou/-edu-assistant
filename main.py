98458733ab530d5a32a34bcfab7cb87961a4bc57
bakaou fofana <bakaoufofana80@gmail.com>
2026-09-08 20:41:53 +0200

    Ajout de la route POST /ask et ignorance de __pycache__

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Question(BaseModel):
+    texte: str
 
 @app.get("/")
 def read_root():
     return {"message": "edu-assistant est en ligne"}
+
+@app.post("/ask")
+def poser_question(question: Question):
+    return {"question_recue": question.texte}
